4a9fb2bb0a16f2062be1b86614bf9e5169141f5b
Pritam Podder <pritampodder12@gmail.com>
2024-08-20 22:19:31 +0530

    AI avatar

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import './Hero.scss';
 
-const avatarUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRHdcFb-AmCIji1yYxkjRnuCTqXvwWo3hbhw&s';
-
 const Hero = () => {
   return (
     <section id="home" className="hero-container">
       {/* <div className="hero-content"> */}
         <h1 className="text-5xl font-bold animate-slide-in">Hi, I'm Pritam</h1>
         <p className="mt-4 text-xl">Full-Stack Developer & Designer</p>
-        <img src={avatarUrl} alt="Pritam Podder" className="avatar-image" />
+        <img src={require('../../assets/images/myPic.jpg')} alt="Pritam Podder" className="avatar-image" />
       {/* </div> */}
     </section>
   );
